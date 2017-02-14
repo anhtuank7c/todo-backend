703b6c7ad4053273e665811935a4baf3981b7ae0
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2017-02-14 10:22:29 +0700

    Initial

---
+++ src/services/task/task-model.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// task-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  content: { type: String, required: true },
+  resolved: { type: Boolean, 'default': false },
+  resolvedAt: { type: Date, 'default': Date.now },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now },
+});
+
+const taskModel = mongoose.model('task', taskSchema);
+
+module.exports = taskModel;
